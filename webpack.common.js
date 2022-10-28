d6d393f86d8f7cc2ac0af50f7727f4511a033b44
vincentt14 <vincent14@student.umn.ac.id>
2022-10-28 13:47:42 +0700

    v02
    Done making Application Shell and setting routes

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
-    filename: 'name.bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
