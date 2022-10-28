23f056483a088f73f0d951df54c17229dc05fa2d
vincentt14 <vincent14@student.umn.ac.id>
2022-10-28 15:24:11 +0700

    v04
    menampilkan konten dari API

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,17 +1,19 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parse';
+import { createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
-      <h2>Detail Page</h2>
+      <div id="movie" class="movie"></div>
     `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
-    const movie = await TheMovieDbSource.detailMovies(url.id);
-    console.log(movie);
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    const movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML = createMovieDetailTemplate(movie);
   },
 };
 
